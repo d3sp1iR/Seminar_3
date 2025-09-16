627dc8549a04671d607f3926cefd27b8312c5282
Sviat4k_07 <magnus.best5@gmail.com>
2025-09-16 17:04:02 +0300

    Doxyfile completed

--- fraction.h
+++ fraction.h
@@ -11,9 +11,19 @@
    };
 
       // Объявляем наши функции
-
+/**
+ * @brief Складывает две дроби
+ * @param a Первая дробь для сложения
+ * @param b Вторая дробь для сложения  
+ * @return Новая дробь, являющаяся результатом сложения
+ */
    Fraction add(Fraction a, Fraction b);
-
+/**
+ * @brief Умножает две дроби
+ * @param a Первый множитель
+ * @param b Второй множитель
+ * @return Произведение дробей
+ */
    Fraction multiply(Fraction a, Fraction b);
 
    void simplify(Fraction &f); // упрощаем переданную дробь
